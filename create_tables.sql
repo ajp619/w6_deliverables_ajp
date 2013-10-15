75688190a6e007542e2dbea5eb337d2fdccee792
ajp619 <ajp619@outlook.com>
2013-10-14 17:02:34 -0700

    Initial commit

---
+++ create_tables.sql
@@ -0,0 +1,38 @@
+CREATE TABLE teachers(
+	teacher_id SERIAL PRIMARY KEY,
+	teacher_name_first text,
+	teacher_name_last text NOT NULL
+);
+
+CREATE TABLE rooms(
+	room_number text PRIMARY KEY
+);
+
+CREATE TABLE classes(
+	class_id SERIAL PRIMARY KEY,
+	teacher_id integer REFERENCES teachers,
+	room_number text REFERENCES rooms,
+	school_year integer
+);
+
+CREATE TABLE students(
+	student_id SERIAL PRIMARY KEY,
+	student_name_first text,
+	student_name_last text NOT NULL
+);
+
+CREATE TABLE class_roll(
+	class_roll_id SERIAL PRIMARY KEY,
+	class_id integer REFERENCES classes,
+	student_id integer REFERENCES students,
+	grade integer
+);
+
+CREATE TABLE laps(
+	laps_id SERIAL PRIMARY KEY,
+	date_completed date,
+	student_id integer REFERENCES students
+);
+	
+	
+	
---
+++ populate_tables.sql
@@ -0,0 +1,17 @@
+INSERT INTO teachers (teacher_name_first, teacher_name_last) VALUES
+	('Melinda', 'Hamill'),
+	('Susan', 'Healy'),
+	('Sara', 'Kirchmeier'),
+	('Lisa', 'Charbonneau'),
+	('Jacqueline', 'Dyer'),
+	('Stephanie', 'Sweeney'),
+	('Maria', 'Weidetz');
+	
+INSERT INTO rooms (room_number) VALUES
+	('D9'),
+	('F5'),
+	('D8'),
+	('F4'),
+	('D7'),
+	('F6'),
+	('F8');
